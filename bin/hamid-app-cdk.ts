a3c1342e67538cda26a5634d7f28f281e4ba6737
Hamid Emamian <hamid.emamian@qlub.io>
2022-06-05 23:40:48 +0300

    cdk app updated

--- bin/hamid-app-cdk.ts
+++ bin/hamid-app-cdk.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { HamidAppCdkStack } from '../lib/hamid-app-cdk-stack';
 import * as utils from '../utils';
 import { HamidAppVpcStack } from '../lib/hamid-app-vpc-stack';
+import { HamidAppDnsStack } from '../lib/hamid-app-dns-stack';
+import { HamidAppAlbStack } from '../lib/hamid-app-alb-stack';
 
 const app = new cdk.App();
 const  configjson = utils.readJSON("./bin/config.json");
@@ -18,26 +19,16 @@ new HamidAppVpcStack(app, "HamidAppVpcStack", {
   enableDnsSupport: configjson.vpc.enableDnsSupport,
 });
 
-new HamidAppCdkStack(app, 'HamidAppCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-/*
-  readonly app_name: string;
-  readonly cidr: string;
-  readonly maxAzs: number;
-  readonly subnets: any[];
-  readonly enableDnsHostnames: boolean;
-  readonly enableDnsSupport: boolean;
-*/
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new HamidAppDnsStack(app, "HamidAppDnsStack", {
+  env: { account: configjson.env.accountnumber, region: configjson.env.region },
+  domainName: configjson.route53.domain,
+  internalDomainNamespace: configjson.route53.internalDomainNamespace,
+  vpc: HamidAppVpcStack.vpc
 });
+
+new HamidAppAlbStack(app, "HamidAppAlbStack", {
+  env: { account: configjson.env.accountnumber, region: configjson.env.region },
+  app_name: configjson.app.name,
+  vpc: HamidAppVpcStack.vpc,
+  sslCertificate: HamidAppDnsStack.sslCertificate
+})
