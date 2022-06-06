45840abbd048a2ffa70b5599685b8d34175705e2
Hamid Emamian <hamid.emamian@qlub.io>
2022-06-06 09:43:33 +0300

    app updated

--- bin/hamid-app-cdk.ts
+++ bin/hamid-app-cdk.ts
@@ -5,6 +5,9 @@ import * as utils from '../utils';
 import { HamidAppVpcStack } from '../lib/hamid-app-vpc-stack';
 import { HamidAppDnsStack } from '../lib/hamid-app-dns-stack';
 import { HamidAppAlbStack } from '../lib/hamid-app-alb-stack';
+import { HamidAppElasicacheStack } from '../lib/hamid-app-elasticache-stack';
+import { HamidAppRdsStack } from '../lib/hamid-app-rds-stack';
+import { HamidAppOpensearchStack } from '../lib/hamid-app-opensearch-stack';
 
 const app = new cdk.App();
 const  configjson = utils.readJSON("./bin/config.json");
@@ -31,4 +34,32 @@ new HamidAppAlbStack(app, "HamidAppAlbStack", {
   app_name: configjson.app.name,
   vpc: HamidAppVpcStack.vpc,
   sslCertificate: HamidAppDnsStack.sslCertificate
-})
+});
+
+new HamidAppElasicacheStack(app, "HamidElasticacheStack", {
+  env: { account: configjson.env.accountnumber, region: configjson.env.region },
+  app_name: configjson.app.name,
+  vpc: HamidAppVpcStack.vpc,
+  name: configjson.redis.name,
+  defaultRedisPassword: configjson.redis.defaultRedisPassword
+});
+
+new HamidAppRdsStack(app, "HamidAppRdsStack", {
+  env: { account: configjson.env.accountnumber, region: configjson.env.region },
+  app_name: configjson.app.name,
+  vpc: HamidAppVpcStack.vpc,
+  name: configjson.rds.name,
+  allocatedStorage: configjson.rds.allocatedStorage
+});
+
+new HamidAppOpensearchStack(app, "HamidAppOpensearchStack", {
+  env: { account: configjson.env.accountnumber, region: configjson.env.region },
+  app_name: configjson.app.name,
+  vpc: HamidAppVpcStack.vpc,
+  name: configjson.opensearch.name,
+  sourceIpPrefixAccess: configjson.vpc.cidr,
+  volumeSize: configjson.opensearch.volumeSize,
+  dataNodeInstanceType: configjson.opensearch.dataNodeInstanceType,
+  dataNodes: configjson.opensearch.dataNodes
+});
+
